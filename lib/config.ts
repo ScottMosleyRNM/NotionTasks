e1065d4f2f2f8a82b41358019c841d547408a230
ScottMosleyRNM <Scott@rightnowmedia.org>
2026-03-26 13:32:23 -0500

    Delete lib directory

--- lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const DATABASE_IDS =
-  process.env.NOTION_DATABASE_ALLOWLIST?.split(",") || [];
-
-export const INBOX_DB = process.env.NOTION_INBOX_DATABASE_ID!;
-export const DEFAULT_ASSIGNEE = process.env.NOTION_DEFAULT_ASSIGNEE_ID!;
