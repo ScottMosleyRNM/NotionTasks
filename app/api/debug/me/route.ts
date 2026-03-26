0d3deda29ba9759d99c6821c0d87402d7d4270a7
ScottMosleyRNM <Scott@rightnowmedia.org>
2026-03-25 20:13:11 -0500

    Delete app directory

--- app/api/debug/me/route.ts
+++
@@ -1,8 +0,0 @@
-import { notion } from "@/lib/notion";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const users = await notion.users.list({});
-
-  return NextResponse.json({ users });
-}
